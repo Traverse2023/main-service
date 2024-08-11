d3677624094a71ba6c5e1002a79049ee07e31abf
Anthony <144615560+Antwanson@users.noreply.github.com>
2024-08-11 10:23:26 -0500

    Added myself as a user

--- src/seedscript.ts
+++ src/seedscript.ts
@@ -124,6 +124,19 @@ const createUsers = async () => {
                     resolve(user.id);
                 }).catch(err => reject(err));
             }),
+	    new Promise<string>((resolve, reject) => {
+                authService.createUser({
+                    pfpUrl: "",
+                    username: "zmyantro@gmail.com",
+                    firstName: "Anthony",
+                    lastName: "Zinsmeyer",
+                    password: plainTextPassword,
+                }).then(user => {
+                    console.log(`Created user: ${JSON.stringify(user)}`)
+                    resolve(user.id);
+                }).catch(err => reject(err));
+            }),
+		
         ])
         return(users);
     } catch (err) {
